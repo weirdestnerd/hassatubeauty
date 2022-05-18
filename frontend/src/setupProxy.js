d287ad23ae96ae129599a699038c2d1d9f806b21
Olu <olu@baller.tv>
2022-05-17 23:13:04 -0400

    setup cors for api, update proxy for FE

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,10 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
+    const target = process.env.NODE_ENV === 'production' ? 'https://hassatubeauty.onrender.com' : 'http://localhost:4000'
+    
     app.use(
         '/api',
         createProxyMiddleware({
-            target: process.env.NODE_ENV === 'production' ? 'https://hassatubeauty.onrender.com' : 'http://localhost:4000',
+            target,
             changeOrigin: true,
             secure: process.env.NODE_ENV === 'production',
         })
