4476b1b5a1c8c85859e1560c8305aab7afedbbe7
Olu <olu@baller.tv>
2022-05-25 15:44:23 -0700

    make preview deploys work

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -1,7 +1,9 @@
-// TODO: update this to include Render's PR preview
-const BASE_API_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://hassatubeauty.onrender.com/api"
-    : "http://localhost:4000/api";
-
+const getBaseApiUrl = () => {
+    switch (process.env.NODE_ENV) {
+        case ("production") return "https://hassatubeauty.onrender.com/api";
+        case ("preview") return process.env.RENDER_BASE_API_URL
+        default return "http://localhost:4000/api"
+    }
+}
+const BASE_API_URL = getBaseApiUrl()
 export default BASE_API_URL;
