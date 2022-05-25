f2a647d655dc495cf00152bf4b84b6e88f7266ef
Olu <olu@baller.tv>
2022-05-25 16:05:03 -0700

    fix prettier, render.yaml, run build

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -1,9 +1,12 @@
 const getBaseApiUrl = () => {
-    switch (process.env.NODE_ENV) {
-        case ("production") return "https://hassatubeauty.onrender.com/api";
-        case ("preview") return process.env.RENDER_BASE_API_URL
-        default return "http://localhost:4000/api"
-    }
-}
-const BASE_API_URL = getBaseApiUrl()
+  switch (process.env.NODE_ENV) {
+    case "production":
+      return "https://hassatubeauty.onrender.com/api";
+    case "preview":
+      return process.env.RENDER_BASE_API_URL;
+    default:
+      return "http://localhost:4000/api";
+  }
+};
+const BASE_API_URL = getBaseApiUrl();
 export default BASE_API_URL;
