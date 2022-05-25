b6bf7032cf928c7f7fec52cde81f17298c459d14
Olu <olu@baller.tv>
2022-05-25 16:15:17 -0700

    ugh line endings

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,7 +1,7 @@
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     // eslint-disable-next-line
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
