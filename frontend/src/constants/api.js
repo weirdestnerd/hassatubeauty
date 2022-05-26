2e2d3b6497272bc877d008dd6f3073fab46b19de
Olu <olu@baller.tv>
2022-05-26 14:37:41 -0700

    still fixing deploys

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -1,5 +1,6 @@
 const getBaseApiUrl = () => {
-  if (process.env.REACT_APP_IN_PREVIEW) return process.env.RENDER_BASE_API_URL;
+  if (process.env.REACT_APP_IN_PREVIEW)
+    return process.env.REACT_APP_RENDER_BASE_API_URL;
   if (process.env.NODE_ENV === "production")
     return "https://hassatubeauty.onrender.com/api";
   return "http://localhost:4000/api";
