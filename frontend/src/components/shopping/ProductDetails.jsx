3812366da308f210e8efec27778e16800cefbf89
Olu <olu@baller.tv>
2022-06-06 19:02:44 -0600

    wip

--- frontend/src/components/shopping/ProductDetails.jsx
+++ frontend/src/components/shopping/ProductDetails.jsx
@@ -3,7 +3,6 @@ import { Disclosure } from "@headlessui/react";
 import classNames from "classnames";
 import { MinusSmIcon, PlusSmIcon } from "@heroicons/react/outline";
 import productType from "../../proptypes/productType";
-import ProductFeatures from "./ProductFeatures";
 
 function ProductDetails({ product }) {
   return (
